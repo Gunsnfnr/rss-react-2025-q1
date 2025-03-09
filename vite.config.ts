5d88a8ba1a2efe3b5992912db11c8de361e1d90e
Gunsnfnr <a.j.p.010203@gmail.com>
2025-03-10 00:57:03 +0300

    chore: add config for react router v7

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react()],
+  plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./app/test/setup.ts'],
     coverage: {
       include: ['**/*.tsx'],
-      exclude: ['**/node_modules/**', '**/*.test.tsx', '**/*.spec.tsx', 'src/__tests__/setup.ts'],
+      exclude: ['**/node_modules/**', '**/*.test.tsx', '**/*.spec.tsx', 'app/__tests__/setup.ts'],
     },
   },
 });
